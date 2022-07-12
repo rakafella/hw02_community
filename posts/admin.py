65ad1ce5e49fc90c99a1bbe6b03473ca857d0fa8
Алексей Романов <rakafella25@gmail.com>
2022-07-12 11:34:21 +0300

    test2

--- posts/admin.py
+++ posts/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
+
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('text', 'pub_date', 'author') 
-    # Добавляем интерфейс для поиска по тексту постов
-    search_fields = ('text',) 
-    # Добавляем возможность фильтрации по дате
-    list_filter = ('pub_date',) 
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
